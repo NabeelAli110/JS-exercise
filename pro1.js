c1f25cea4387a1cb7e8a8f8aeb9ecb2ebfca290f
Nabeel Ahmed Magsi <nabeelAli.magsi36@gmail.com>
2024-09-11 17:28:11 +0500

    Add files via upload

---
+++ pro1.js
@@ -0,0 +1,103 @@
+// first exercise
+
+alert("pleace enter a valide pasword")
+alert("welcome to the jas world\n happy coding")
+alert("wellcome to js world")
+alert("happy coding\n prevent this page from creating editional dialogues")
+
+// exercise 2nd
+
+var username;
+
+var myname = "Nabeel Ahmed Magsi";
+
+var message = "Hello World";
+
+alert(message);
+
+alert("myname");
+
+var age = 33;
+
+alert(age + " years old");
+
+var certificate = "Certified Web and Applications Developer";
+
+alert(certificate);
+
+var p = "PIZZA";
+
+alert(p + "\nPIZZ\nPIZ\nPI\nP");
+
+var email = "nabeel.mbaloch6@gmail.com";
+var text = "my email address is";
+alert(text + email);
+
+var book = "A Smarter Way To Learn JAvascript";
+var btext = "I am Learning From ";
+alert(btext + book);
+
+document.body.innerHTML = "yeh! i can write html content through HTML";
+
+alert("▬▬▬▬▬▬▬▬▬ஜ۩۞۩ஜ▬▬▬▬▬▬▬▬▬");
+
+// 3rd ex
+
+var age = 33;
+alert("i am " + age + " years old");
+
+var birthyear = 1990;
+document.body.innerHTML = "My Bith Year is " + birthyear + "\n data type of my decleared variale is Number";
+
+var custommerName = "john Doe";
+var items = "t-shirts";
+var num = 4;
+var storeName = "xyzClothing.com";
+
+document.body.innerHTML = custommerName + " brought " + num + " " + items + " from " + storeName;
+
+//  exercise 4th
+
+var variable1, variable2, variable3;
+
+
+// Declare 5 legal & 5 illegal variable names.
+
+var v1, v2, v3, v4, v5;
+
+var 1v, .2v, *v3, @v4, v@5; 
+
+document.body.innerHTML = "<b>Rules for naming js variables</b>";
+
+document.body.innerHTML = "variables only contains numbers, letters, $ and _. For example $my_1stVariable";
+
+document.body.innerHTML = "Variables must begin with a letter, $ or _. For example $name, _name or name";
+
+document.body.innerHTML = "Variable names are case sensitive";
+
+document.body.innerHTML = "Variable names should not be JS keywords";
+
+// exercise 5th
+
+var num1 = 3;
+var num2 = 5;
+var num3 = num1 + num2;
+document.body.innerHTML = num1 + " + " + num2 +" = " + num3;
+
+var num1 = 3;
+var num2 = 5;
+var num3 = num1 - num2;
+document.body.innerHTML = num1 + " - " + num2 +" = " + num3;
+
+var num1 = 3;
+var num2 = 5;
+var num3 = num1 * num2;
+document.body.innerHTML = num1 + " * " + num2 +" = " + num3;
+
+var num1 = 3;
+var num2 = 5;
+var num3 = num1 / num2;
+document.body.innerHTML = num1 + " / " + num2 +" = " + num3;
+
+// 6th exercise
+
